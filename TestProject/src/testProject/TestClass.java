51c5ed543bdcdbffe51a2cf48688c03de9ed9951
SnPatil25 <128477060+SnPatil25@users.noreply.github.com>
2023-03-21 21:39:49 +0530

    Basic Task Commit

---
+++ TestProject/src/testProject/TestClass.java
@@ -0,0 +1,16 @@
+package testProject;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("TestBase Class");
+		System.out.println("PageLayer Class");
+		System.out.println("TestLayerClass");
+		System.out.println("Util Class");
+		
+		
+
+	}
+
+}
